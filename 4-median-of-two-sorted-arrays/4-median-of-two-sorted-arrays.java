964f030b6a44095260818be08b0e8f44da499de2
Bharat Prajapati <99390274+bharatprajapati111@users.noreply.github.com>
2022-08-25 22:11:07 +0530

    Time: 10 ms (18.59%), Space: 50.8 MB (12.95%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) 
+    {
+        int m=nums1.length;
+        int n=nums2.length;
+        int arr[]=new int[m+n];
+        for(int i=0;i<m;i++)
+        {
+            arr[i]=nums1[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            arr[m+i]=nums2[i];
+        }
+        Arrays.sort(arr);
+        int mid=arr.length/2;
+        if(arr.length%2==0) 
+        {
+	        return (double)(arr[mid]+arr[mid-1])/2;
+        }
+        else 
+        {
+	        return (double)arr[mid];
+        }
+    }
+}
