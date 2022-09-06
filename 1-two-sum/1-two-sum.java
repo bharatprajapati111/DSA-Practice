3af3fc8d08df56c2fc29e072534de41fa0ae9f62
Bharat Prajapati <99390274+bharatprajapati111@users.noreply.github.com>
2022-09-06 08:34:01 +0530

    Time: 3 ms (92.60%), Space: 45.7 MB (37.84%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,6 +1,54 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) 
     {
+        int [] result=new int[2];
+        Map map=new HashMap();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!map.containsKey(target-nums[i]))
+            {
+                map.put(nums[i],i);
+            }
+            else
+            {
+                result[1]=i;
+                result[0]=(int)map.get(target-nums[i]);
+                return result;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+    public int[] twoSum(int[] nums, int target) 
+    {
         for(int i=0;i<nums.length-1;i++) //traverse the array for 1st number
         {
             for(int j=i+1;j<nums.length;j++) //traverse the array for 2nd number
@@ -13,4 +61,4 @@ class Solution {
         }
         return null;
     }
-}
+}*/
