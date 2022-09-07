b9369959d2233ded777d6167c0cdd23dbec7798b
Bharat Prajapati <99390274+bharatprajapati111@users.noreply.github.com>
2022-09-07 08:49:15 +0530

    Time: 4 ms (34.01%), Space: 41.5 MB (72.08%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,6 +1,57 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) 
     {
+        int n=arr.length;
+        
+        int total=0;
+        
+        for(int i=1;i<n+1;i+=2){
+            int sum=0;
+            
+            for(int j=0;j<i;j++){
+                sum+=arr[j];
+            }
+            
+            total+=sum;
+            
+            int j=0,k=i;
+            
+            while(k<n){
+                sum-=arr[j++];
+                sum+=arr[k++];
+                
+                total+=sum;
+            }
+        }
+        
+        return total;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+    public int sumOddLengthSubarrays(int[] arr) 
+    {
         int n = arr.length;
         int sum = 0;
         for (int i=0; i<n; i++) 
@@ -9,4 +60,4 @@ class Solution {
         }
         return sum;
     }
-}
+}*/
